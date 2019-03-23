0f75dd0a9d15a7904ba92aea21e3a25a92996657
KingArt4444 <artem_korolev13@mail.ru>
2019-03-23 22:20:51 +0300

    add working code

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  var triang = [];
+  for(i = 0;i<preferences.length; i++){
+      var first = preferences[i];
+      var second = preferences[first-1];
+      var third = preferences[second-1];      
+      if(third == (i+1)){
+      count++;
+      }
+  }
+    return Math.floor(count/3);
 };
